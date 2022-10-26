327e3e7655fa390befe3ded1f2fdacbf4e349f8f
cactys95 <cactys95@yandex.ru>
2022-10-26 23:16:38 +0300

    feat: добавить "О проекте"/"Технологии" + адаптив

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,5 +1,7 @@
 import AboutProject from '../AboutProject/AboutProject';
+import Portfolio from '../Portfolio/Portfolio';
 import Promo from '../Promo/Promo';
+import Techs from '../Techs/Techs';
 import './Main.css';
 
 const Main = () => {
@@ -7,6 +9,8 @@ const Main = () => {
       <div className='content'>
         <Promo />
         <AboutProject />
+        <Techs />
+        <Portfolio />
       </div>
   )
 }
---
+++ src/components/Portfolio/Portfolio.js
@@ -0,0 +1,9 @@
+import './Portfolio.css'
+
+const Portfolio = () => {
+  return (
+    <div>Portfolio</div>
+  )
+}
+
+export default Portfolio
