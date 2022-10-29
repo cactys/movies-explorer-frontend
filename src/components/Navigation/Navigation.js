5c52ae0a2d0829d480c893821e9b25252abaeb4e
cactys95 <cactys95@yandex.ru>
2022-10-30 00:18:49 +0300

    feat: добавить кнопку бургер

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,26 +5,37 @@ import profileIcon from '../../images/profile-icon.svg';
 const Navigation = () => {
   return (
     <nav className="navigation">
-      <div className="navigation__auth">
-        <NavLink
-          to="/movies"
-          className="navigation__link"
-          activeClassName="navigation__link_active"
-        >
-          Фильмы
-        </NavLink>
-        <NavLink
-          to="/saved-movies"
-          className="navigation__link"
-          activeClassName="navigation__link_active"
-        >
-          Сохранённые фильмы
-        </NavLink>
+      <div className="navigation__main">
+        <div className="navigation__auth">
+          <NavLink
+            to="/movies"
+            className="navigation__link"
+            activeClassName="navigation__link_active"
+          >
+            Фильмы
+          </NavLink>
+          <NavLink
+            to="/saved-movies"
+            className="navigation__link"
+            activeClassName="navigation__link_active"
+          >
+            Сохранённые фильмы
+          </NavLink>
+        </div>
+        <Link to="/profile" className="navigation__profile">
+          Аккаунт{' '}
+          <img
+            className="navigation__profile-icon"
+            src={profileIcon}
+            alt="Аккаунт"
+          />
+        </Link>
       </div>
-      <Link to="/profile" className="navigation__profile">
-        Аккаунт{' '}
-        <img className="navigation__profile-icon" src={profileIcon} alt="Аккаунт" />
-      </Link>
+      <div className='navigation__burger'>
+        <div className='navigation__burger-btn'>
+          <span />
+        </div>
+      </div>
     </nav>
   );
 };
