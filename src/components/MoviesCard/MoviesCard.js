c4ca654f614f72726e5d6a1599c3e2f0358f3563
cactys95 <cactys95@yandex.ru>
2022-11-02 23:13:33 +0300

    просто сохранил ссылку на стэковерфлов

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -9,13 +9,14 @@ const MoviesCard = ({ card, mark }) => {
           <h2 className="movies-card__title">{card.title}</h2>
           <p className="movies-card__duration">{card.duration}</p>
         </div>
-        <div
-          className={`movies-card__mark movies-card__mark_${mark}`}
-        />
+        <div className={`movies-card__mark movies-card__mark_${mark}`} />
       </div>
       <img className="movies-card__image" alt={card.duration} src={card.image} />
     </div>
   );
 };
+
+// кнопка еще
+// https://translated.turbopages.org/proxy_u/en-ru.ru.fd2e2d2c-6362ce0c-67a5d0be-74722d776562/https/stackoverflow.com/questions/64284221/load-more-button-in-react
 
 export default MoviesCard;
