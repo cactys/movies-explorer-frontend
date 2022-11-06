25444a8dec2ee1945380723786a7bddf86ce2483
cactys95 <cactys95@yandex.ru>
2022-11-06 21:46:57 +0300

    feat: переделать логотип без использования картинки

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,14 +1,11 @@
 import { Link, Route } from 'react-router-dom';
 import './Logo.css';
-import logo from '../../images/logo.svg'
 
 const Logo = () => {
   return (
     <div className="logo">
       <Route path="/">
-        <Link className="logo__link" to="/">
-          <img src={logo} className="logo__img" alt="Логотип" />
-        </Link>
+        <Link className="logo__link" to="/" />
       </Route>
     </div>
   );
