84fcb0168b83bbc4fde716fca3b6404db5eec52c
cactys95 <cactys95@yandex.ru>
2022-11-21 16:56:33 +0300

    fix: URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 export const URL = {
   beatfilmMovies: 'https://api.nomoreparties.co',
-  // mainApi: 'https://api.khnychkov.nomoredomains.icu',
-  localApi: 'http://localhost:3030',
+  mainApi: 'https://api.khnychkov.nomoredomains.icu',
+  // localApi: 'http://localhost:3030',
 };
 
 export const PAGE_SIZE = {
